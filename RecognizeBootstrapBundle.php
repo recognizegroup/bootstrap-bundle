61357e57ea05e8c70dfc7d325959a5e2680946a8
nobermeijer <n.obermeijer@recognize.nl>
2014-08-08 14:22:35 +0200

    Initial commit

---
+++ RecognizeBootstrapBundle.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Recognize\BootstrapBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * Class RecognizeBootstrapBundle
+ * @package Recognize\BootstrapBundle
+ * @author Nick Obermeijer <n.obermeijer@recognize.nl>
+ */
+class RecognizeBootstrapBundle extends Bundle {
+
+}
